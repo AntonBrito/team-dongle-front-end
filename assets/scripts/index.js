c1077248a5fd87bda7186f3399a43fe52e062ad7
hadleyana <hadleysweck@gmail.com>
2017-09-14 10:21:11 -0400

    Added click Handlers
    we can now click buttons

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
-
+const events = require('./auth/events.js')
 $(() => {
   setAPIOrigin(location, config)
 })
@@ -10,7 +10,10 @@ $(() => {
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 $(() => {
-  $('')
+  $('#sign-up').on('submit', events.onSignUp)
+  $('#sign-in').on('submit', events.onSignIn)
+  $('#sign-out').on('submit', events.onSignOut)
+  $('#changePassword').on('submit', events.onChangePassword)
 })
 // use require without a reference to ensure a file is bundled
 // require('./example')
