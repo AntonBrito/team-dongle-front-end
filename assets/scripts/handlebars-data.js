145144b8d63c828fb4b8ad5181f03e5d8cbd591a
hadleyana <hadleysweck@gmail.com>
2017-09-15 16:33:30 -0400

    Get All Posts HB
    added get all posts functionality to hb's functions

--- assets/scripts/handlebars-data.js
+++ assets/scripts/handlebars-data.js
@@ -1,19 +1,19 @@
 // ///////////////////////////
 // template context for posts/
 // ///////////////////////////
-$(() => {
+const postsHandlebars = function (title, body) {
   const postHandlebarsTemplate = require('./templates/posts-template.handlebars')
   const context = {
-    'title': "I'm a Title!",
-    'body': 'I HAVE BODY'
+    'title': title,
+    'body': body
   }
   const postTemplate = postHandlebarsTemplate(context)
   $('#post-div').append(postTemplate)
-})
+}
 // ///////////////////////////
 // template context for pages/
 // ///////////////////////////
-$(() => {
+const pagesHandlebars = function () {
   const pageHandlebarsTemplate = require('./templates/pages-template.handlebars')
   const context = {
     'title': "I'm a Title!",
@@ -23,15 +23,21 @@ $(() => {
   }
   const html = pageHandlebarsTemplate(context)
   $('#pages-div').append(html)
-})
+}
 // ////////////////////////////
 // template context for drpdwn/
 // ////////////////////////////
-$(() => {
+const dropdownHandlebars = function () {
   const pageHandlebarsTemplate = require('./templates/pages-dropdown-list.handlebars')
   const context = {
     'title': "I'm a Title!"
   }
   const html = pageHandlebarsTemplate(context)
   $('#pages-div').append(html)
-})
+}
+
+module.exports = {
+  postsHandlebars,
+  pagesHandlebars,
+  dropdownHandlebars
+}
